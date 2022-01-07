3711f60dbe330d887d2967abc6fb62555efde5d0
vgavhane1 <vaibhav.gavhane9@gmail.com>
2022-01-07 13:28:10 +0530

    customized json response body

--- src/main/java/com/rebobank/customer_statement_processor/exception/RestExceptionHandler.java
+++ src/main/java/com/rebobank/customer_statement_processor/exception/RestExceptionHandler.java
@@ -3,10 +3,15 @@ package com.rebobank.customer_statement_processor.exception;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import net.minidev.json.JSONObject;
 /**
  *  Global exception handler for all exceptions which will finally send to Http Client.
  *  @author vgavhane
@@ -16,13 +21,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(CustomerStatementProcessorException.class)
-	protected ResponseEntity<Object> handleDiscoveryException(
+	protected ResponseEntity<Object> handleCustomerStatementProcessorException(
 			CustomerStatementProcessorException ex) {
 		return buildResponseEntity(ex);
 	}
 	
 	private ResponseEntity<Object> buildResponseEntity(CustomerStatementProcessorException customerStatementProcessorException) {
-		return new ResponseEntity<>(customerStatementProcessorException, customerStatementProcessorException.getStatus());
+		
+		HttpHeaders httpResponseHeaders = new HttpHeaders();
+		httpResponseHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpResponseHeaders.set("X-ERROR","true");
+		
+		JSONObject resonseBody = new JSONObject();
+		resonseBody.put("message", customerStatementProcessorException.getMessage());
+		resonseBody.put("status", customerStatementProcessorException.getStatus());
+		
+		return new ResponseEntity<>(resonseBody, httpResponseHeaders, HttpStatus.BAD_REQUEST);
 	}
 	
 	@ExceptionHandler(UserAuthenticationException.class)

61cf60fd17fef6dedb5e6b19845103b7e94a1b6e
vgavhane1 <vaibhav.gavhane9@gmail.com>
2022-01-07 12:28:52 +0530

    Running Junit using SpringBootTest

--- src/test/java/com/rebobank/customer_statement_processor/CustomerStatementProcessorApplicationTests.java
+++ src/test/java/com/rebobank/customer_statement_processor/CustomerStatementProcessorApplicationTests.java
@@ -4,26 +4,20 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.rebobank.customer_statement_processor.dto.CustomerStatementProcessorReportDto;
 import com.rebobank.customer_statement_processor.model.CustomerStatementRecord;
 import com.rebobank.customer_statement_processor.service.CustomerStatementProcessorService;
-import com.rebobank.customer_statement_processor.service.CustomerStatementProcessorServiceImpl;
 
-@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CustomerStatementProcessorApplicationTests {
 
+	@Autowired
 	public CustomerStatementProcessorService customerStatementProcessorService;
-
-	@Before
-	public void setUp(){
-		customerStatementProcessorService = new CustomerStatementProcessorServiceImpl();
-	}
 
 	@Test
 	public void testValidateEndBalance(){
@@ -39,9 +33,9 @@ public class CustomerStatementProcessorApplicationTests {
 		customerStatementRecords.add(customerStatementRecord);
 
 		customerStatementProcessorReportDto = customerStatementProcessorService.validateEndBalance(customerStatementRecords, customerStatementProcessorReportDto);
-		Assert.assertEquals(customerStatementProcessorReportDto.getReport().size(), 0);
+		Assertions.assertEquals(customerStatementProcessorReportDto.getReport().size(), 0);
 	}
-	
+
 	@Test
 	public void testWrongValidateEndBalance(){
 		List<CustomerStatementRecord> customerStatementRecords = new ArrayList<>();
@@ -56,9 +50,9 @@ public class CustomerStatementProcessorApplicationTests {
 		customerStatementRecords.add(customerStatementRecord);
 
 		customerStatementProcessorReportDto = customerStatementProcessorService.validateEndBalance(customerStatementRecords, customerStatementProcessorReportDto);
-		Assert.assertEquals(customerStatementProcessorReportDto.getReport().size(), 1);
+		Assertions.assertEquals(customerStatementProcessorReportDto.getReport().size(), 1);
 	}
-	
+
 	@Test
 	public void testValidateUniqueTransactionReferences(){
 		List<CustomerStatementRecord> customerStatementRecords = new ArrayList<>();
@@ -69,7 +63,7 @@ public class CustomerStatementProcessorApplicationTests {
 		customerStatementRecord.setMutation(1234.00);
 		customerStatementRecord.setTransactionReference(1234l);
 		customerStatementRecord.setEndBalance(2468.00);
-		
+
 		CustomerStatementRecord customerStatementRecord1 = new CustomerStatementRecord();
 		customerStatementRecord1.setTransactionReference(12345l);
 
@@ -77,7 +71,7 @@ public class CustomerStatementProcessorApplicationTests {
 		customerStatementRecords.add(customerStatementRecord1);
 
 		customerStatementProcessorReportDto = customerStatementProcessorService.validateUniqueTransactionReferences(customerStatementRecords, customerStatementProcessorReportDto);
-		Assert.assertEquals(customerStatementProcessorReportDto.getReport().size(), 0);
+		Assertions.assertEquals(customerStatementProcessorReportDto.getReport().size(), 0);
 	}
 
 	@Test
@@ -90,7 +84,7 @@ public class CustomerStatementProcessorApplicationTests {
 		customerStatementRecord.setMutation(1234.00);
 		customerStatementRecord.setTransactionReference(1234l);
 		customerStatementRecord.setEndBalance(2468.00);
-		
+
 		CustomerStatementRecord customerStatementRecord1 = new CustomerStatementRecord();
 		customerStatementRecord1.setTransactionReference(1234l);
 
@@ -98,6 +92,6 @@ public class CustomerStatementProcessorApplicationTests {
 		customerStatementRecords.add(customerStatementRecord1);
 
 		customerStatementProcessorReportDto = customerStatementProcessorService.validateUniqueTransactionReferences(customerStatementRecords, customerStatementProcessorReportDto);
-		Assert.assertEquals(customerStatementProcessorReportDto.getReport().size(), 1);
+		Assertions.assertEquals(customerStatementProcessorReportDto.getReport().size(), 1);
 	}
 }
